4c4d5f298c7c17e8df7f4a7091637d143fa3b0c6
nikuha <yoginika@yandex.ru>
2022-02-08 16:10:30 +0300

    lesson3 basket

--- js/main.js
+++ js/main.js
@@ -53,30 +53,95 @@ class ProductItem {
 }
 
 class Basket {
-    container = ''; // где отображаем корзину
-    items = []; // содержимое корзины, элементы BasketItem
+    items = [];
 
-    constructor(container){} // инициируем корзину
+    constructor(container = '.basket-menu', link_selector = '.basket-a'){
+        this.container = document.querySelector(container);
+        this._initLink(link_selector);
+        this._fetchItems().then(() => this._render());
+    }
 
-    addItem(item){} // добавить товар
-    deleteItem(item){} // удалить товар
+    _initLink(link_selector){
+        document.querySelector(link_selector).addEventListener('click', () => {
+            this.container.classList.toggle('active');
+        });
+    }
 
-    getTotalCost(){} // сумма всех товаров
+    _fetchItems(){
+        return fetch(`${API_URL}/getBasket.json`)
+            .then(result => result.json())
+            .then(data => {
+                for (let item of data.contents) {
+                    this.items.push(new BasketItem(item));
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
-    render(){} // отобразить корзину
+    _render(){
+        for (let item of this.items) {
+            this.container.insertAdjacentHTML("beforeend", item.render());
+        }
+        this._initButtons();
+    }
+
+    _initButtons(){
+        this.container.querySelectorAll(BasketItem.getDeleteSelector()).forEach(button => {
+            button.addEventListener('click',  () => {
+                const item_id = Number(button.getAttribute('data-id'));
+                const new_items = [];
+                this.items.forEach((item, k) => {
+                    if(item.id === item_id){
+                        this.container.querySelector(item.getSelector()).remove();
+                    } else{
+                        new_items.push(item);
+                    }
+                })
+                this.items = new_items;
+                if(!this.items.length){
+                    this.container.classList.remove('active');
+                }
+            })
+        });
+    }
+
 }
 
 class BasketItem {
-    product; // привязка к ProductItem
-    amount; // кол-во продуктов в корзине
 
-    constructor(product, amount = 1){} // инициируем корзину
+    constructor(item){
+        ({ product_name: this.title, price: this.price, id_product: this.id, quantity: this.quantity } = item);
+        this.img = item.img ? item.img : 'img/default_s.png';
+    }
 
-    updateAmount(amount){} // изменить количество
+    static getDeleteSelector(){
+        return '.item-container button.delete';
+    }
 
-    getCost(){} // стоимость товаров (product.price*amount)
+    getSelector(){
+        return `.item-container[data-id="${this.id}"]`;
+    }
 
-    render(){} // отобразить элемент корзины
+    render() {
+        return `
+            <div class="item-container row space-between" data-id="${this.id}">
+                <div><img src="${this.img}" alt=""></div>
+                <div class="name">
+                    <div>${this.title}</div> 
+                    <div>${this.price}&#8381;</div>   
+                </div>
+                <div>
+                    <div>Кол-во:</div> 
+                    <div>${this.quantity}</div> 
+                </div>
+                <div>
+                    <div><button class="standard red delete" data-id="${this.id}">Удалить</button></div> 
+                </div>
+            </div>`;
+    }
 }
 
 let list = new ProductList();
+const basket = new Basket();
